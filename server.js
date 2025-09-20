7f9b79b8219d608a67cc81e1f5198c145ec5d16a
Raghvendra Misra <itsraghav12@gmail.com>
2025-09-21 02:28:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ app.get("/screenshot", async (req, res) => {
   }
 
   try{
-    const img = await captureWebsite.file('https://sindresorhus.com', 'screenshot.png');
+    const img = await captureWebsite.file(url, 'screenshot.png');
  const filePath = path.join(__dirname, 'screenshot.png'); // Construct absolute path
       res.status(200).sendFile(filePath);
   }catch(e) {
     console.error(e)
+    res.status(500).send(e)
   }
   
 });
